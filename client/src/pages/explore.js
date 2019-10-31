65058b5dd42fe47281926ad0f87991c6e6da6f2d
danielbrog <brog.daniel@gmail.com>
2019-10-31 17:34:24 -0400

    Fixed logic behind infinite scroll
    continues to add  to the frontend projects until 404 or doesn't need more(page full)

--- client/src/pages/explore.js
+++ client/src/pages/explore.js
@@ -8,9 +8,6 @@ import {
   MenuItem
 } from "@material-ui/core";
 import classNames from "classnames";
-import coffeeCup from "../assets/coffee-cup.jpg";
-import espresso from "../assets/espresso.jpg";
-import pouringCoffee from "../assets/pouring-coffee.jpg";
 import ProjectList from "./Project";
 import authFetch from "../utilities/auth";
 import InfiniteScroll from "react-infinite-scroller";
@@ -33,105 +30,12 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const mockProjectData = [
-  {
-    id: "1",
-    img: coffeeCup,
-    category: "Food and Craft",
-    alt: "Coffee Cup",
-    industry: "Customer Service",
-    title: "Urban Jungle: eco-friendly coffee shop",
-    raised_amount: "23,874",
-    funding_goal: "40,000",
-    equity: "10%",
-    daysToGo: "44",
-    authorName: "John Snow",
-    location: "Pripyat, Ukraine",
-    src: ""
-  },
-  {
-    id: "2",
-    img: espresso,
-    category: "Food and Craft",
-    alt: "Espresso",
-    industry: "Coffee",
-    title: "Energy Run: the quickest coffee experience",
-    raised_amount: "7,257",
-    funding_goal: "12,383",
-    equity: "13%",
-    daysToGo: "19",
-    authorName: "Jerry",
-    location: "NYC, NY",
-    src: ""
-  },
-  {
-    id: "3",
-    img: pouringCoffee,
-    category: "Life Hacks",
-    alt: "Pouring Coffee",
-    industry: "Restaurant",
-    title: "Energy Rush: an even quicker coffee experience",
-    raised_amount: "34,912",
-    funding_goal: "50,000",
-    equity: "5%",
-    daysToGo: "5",
-    authorName: "Jerry",
-    location: "NYC, NY",
-    src: ""
-  },
-  {
-    id: "4",
-    img: pouringCoffee,
-    category: "Life Hacks",
-    alt: "Pouring Coffee",
-    industry: "Restaurant",
-    title: "Energy Rush: an even quicker coffee experience",
-    raised_amount: "34,912",
-    funding_goal: "50,000",
-    equity: "5%",
-    daysToGo: "5",
-    authorName: "Jerry",
-    location: "NYC, NY",
-    src: ""
-  },
-  {
-    id: "5",
-    img: pouringCoffee,
-    category: "Life Hacks",
-    alt: "Pouring Coffee",
-    industry: "Restaurant",
-    title: "Energy Rush: an even quicker coffee experience",
-    raised_amount: "34,912",
-    funding_goal: "50,000",
-    equity: "5%",
-    daysToGo: "5",
-    authorName: "Jerry",
-    location: "NYC, NY",
-    src: ""
-  },
-  {
-    img: pouringCoffee,
-    category: "Life Hacks",
-    alt: "Pouring Coffee",
-    industry: "Restaurant",
-    title: "Energy Rush: an even quicker coffee experience",
-    raised_amount: "34,912",
-    funding_goal: "50,000",
-    equity: "5%",
-    daysToGo: "5",
-    authorName: "Jerry",
-    location: "NYC, NY",
-    src: ""
-  }
-];
-
 function Explore() {
   const classes = useStyles();
-
   const [projects, setProjects] = useState([]);
-  const [industries, setIndustries] = useState([]);
   const [hasMore, setHasMore] = useState(true);
   const [cursor, setCursor] = useState(0);
+  const [uniqueIndustries, setUniqueIndustries] = useState(new Set());
 
   //console.log(projects)
 
@@ -146,21 +50,15 @@ function Explore() {
 
   //This loads the industries from projects
   useEffect(() => {
-    const uniqueIndustries = new Set();
-
     projects.forEach(project => {
-      //REMEMBER forEach is for arrays (for each element)
-      uniqueIndustries.add(project.industry); //Adding each project's industry in the Set
+      setUniqueIndustries(uniqueIndustries.add(project.industry)); //Adding each project's industry in the Set
     });
-
-    setIndustries(Array.from(uniqueIndustries)); //This adds to 'industry' state hook by forming an array by iterating over an OBJECT because the initial includes an empty array!!
-  }, [projects]); //THIS MEANS if projects array changes, it will render
+  }, [projects]); //projects array changes, it will rerender
 
   const onChangeFilter = event => {
-    //This is used in the Select element for industry... think 'event' for 'e'
-
-    const { value, name } = event.target; // IOW, the 'event' (or 'e') is used to create an object of 2 props from the element that hosts the event... such as clicking on allows you to get the value and name which are parts of the <Select> element;
-    setFilterQuery({ ...filterQuery, [name]: value }); //This will add onto the filterQuery but replaces the key-pair value... IOW ['industry']: one of the options avalaible due to <MenuItem>
+    setHasMore(true); //download more objects incase it's needed
+    const { value, name } = event.target;
+    setFilterQuery({ ...filterQuery, [name]: value });
   };
 
   const clearing = () => {
@@ -171,7 +69,6 @@ function Explore() {
 
   const filterProjects = projects => {
     const { industry, deadline, location } = filterQuery; //TODO using deadline yet, it should the project timestamp and subtract dates
-    //Shouldn't deadline's & location's positions be FLIPPED??
 
     //Check issue # ...
     return projects.filter(
@@ -186,16 +83,17 @@ function Explore() {
   };
 
   const loadMore = () => {
-    console.log("/projects?pageNo=" + cursor + "&size=3");
+    setHasMore(false);
+    const url = "/projects?pageNo=" + cursor + "&size=3";
     authFetch({
-      url: "/projects?pageNo=" + cursor + "&size=3"
+      url
     }).then(res => {
-      if (res.error || res.status == 304) {
-        console.log(res);
+      if (res.error) {
         setHasMore(false);
       } else {
         setProjects(projects.concat(res));
         setCursor(cursor + 1);
+        setHasMore(true);
       }
     });
   };
@@ -217,7 +115,7 @@ function Explore() {
           }
         >
           <MenuItem value={""}>{}</MenuItem>
-          {industries.map(industry => (
+          {[...uniqueIndustries].map(industry => (
             <MenuItem value={industry}>{industry}</MenuItem>
           ))}
         </Select>
